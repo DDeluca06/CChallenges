f809c7232c5b7c2171ee5533a55aaea4ed4be356
DDeluca06 <ddelucalyons@gmail.com>
2024-11-07 10:59:54 -0500

    Shortage detection

--- array.js
+++ array.js
@@ -1,24 +1,44 @@
-// Data Const
+// This is our data variable, treat it with care!
 const cyberData = [
   {
     Level: "Entry-level",
-    LocalJobOpenings: 15,
+    LocalJobOpenings: 3567,
     AvailableTalent: 50,
   },
   {
     Level: "Mid-level",
-    LocalJobOpenings: 10,
+    LocalJobOpenings: 9558,
     AvailableTalent: 30,
   },
   {
     Level: "Senior-level",
-    LocalJobOpenings: 5,
+    LocalJobOpenings: 13432,
     AvailableTalent: 10,
   },
 ];
 
-// Displaying it
-cyberData.forEach(role => { // For each 'role' in the table...
-    console.log(`${role.Level}: ${role.LocalJobOpenings} openings`); // ... display how many openings there are.
+// Grab the combined total of role openings
+const totalOpenRoles = cyberData.reduce((total, job) => total + job.LocalJobOpenings, 0);
+
+// Display to our user in console
+cyberData.forEach((job) => {
+  // For each 'role' in the table...
+  console.log(`${job.Level}: ${job.LocalJobOpenings} openings`); // ... display how many openings there are.
 });
-console.log("I don't know what you wanted me to 'calculate'.")
+
+// Total open roles
+console.log(`There is a combined total of ${totalOpenRoles} open roles.`);
+
+// Find our largest shortage by cycling through our current list
+function findLargestShortage() {
+  // If we wanted to improve this, work with reduce. But that's a PAIN for me to
+  // do right now. It'll work for demonstration!
+  let largest = Math.max(
+    cyberData[0].LocalJobOpenings,
+    cyberData[1].LocalJobOpenings,
+    cyberData[2].LocalJobOpenings
+  );
+  let role = cyberData.find(item => item.LocalJobOpenings === largest).Level;
+  return `The role with the largest shortage is ${role} with ${largest} job openings.`;
+}
+console.log(findLargestShortage(cyberData));
